91e0ffb968c62b4e3e34ce2a9696c1050459c69e
Matias Rodriguez <rodriguez.matias.g@gmail.com>
2022-08-02 10:07:43 -0300

    01 homework

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,13 +1,29 @@
 'use strict'
 
 function BinarioADecimal(num) {
-  // tu codigo aca
-
+  let dec = 0;
+  let largo = (num.length-1)
+  for (let i = 0 ; i < num.length ; i++){
+    if ( num[i] == 1){
+      dec = (Math.pow(2, (largo-i)))  + dec
+    } 
+  }
+return dec
 }
 
 function DecimalABinario(num) {
-  // tu codigo aca
-
+  let resto = 0;
+  let arr = [];
+  if (num === 0){
+    div = "0";
+  }
+  while (num>0) {
+    resto = num%2
+    arr.unshift(resto)
+    num = Math.floor(num/2)
+  }
+  let div = arr.join('')
+  return div
 }
 
 
